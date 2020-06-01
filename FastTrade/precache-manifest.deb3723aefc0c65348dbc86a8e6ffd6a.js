efcb6912efc90db1845b62c8f5073b9eec6bd0d8
ManiVannan Murugesan <mmvmaniit@gmail.com>
2020-06-01 23:36:23 +0530

    Add files via upload

---
+++ FastTrade/precache-manifest.deb3723aefc0c65348dbc86a8e6ffd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9811006c8f8bb1b984937dcfd5627e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed386bd2f5ec196e6f1",
+    "url": "/static/css/main.d94cf6d8.chunk.css"
+  },
+  {
+    "revision": "6df8f0af90cf168577e0",
+    "url": "/static/js/2.77b057da.chunk.js"
+  },
+  {
+    "revision": "5b5ab23b2fa050e49d520509df620f9d",
+    "url": "/static/js/2.77b057da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed386bd2f5ec196e6f1",
+    "url": "/static/js/main.ded4a120.chunk.js"
+  },
+  {
+    "revision": "f52a4c9f33b6ff780c64",
+    "url": "/static/js/runtime-main.baba09e8.js"
+  }
+]);
