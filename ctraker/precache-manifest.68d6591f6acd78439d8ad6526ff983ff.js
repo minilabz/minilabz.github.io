fd048c497ec4352f6940a116f54202735d27d15d
ManiVannan Murugesan <mmvmaniit@gmail.com>
2020-06-16 03:20:30 +0530

    Add files via upload

---
+++ ctraker/precache-manifest.68d6591f6acd78439d8ad6526ff983ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8938bb9e5c7df02cf28cea3aa53a085d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67b62d0c65e1d30e3c93",
+    "url": "/static/css/main.d94cf6d8.chunk.css"
+  },
+  {
+    "revision": "1e14903b3f645313c91c",
+    "url": "/static/js/2.8febc836.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8febc836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b62d0c65e1d30e3c93",
+    "url": "/static/js/main.6d92dbde.chunk.js"
+  },
+  {
+    "revision": "b6db8c21049841b1d2a0",
+    "url": "/static/js/runtime-main.af1f34e3.js"
+  },
+  {
+    "revision": "2fbba9bfbebb7ba06e5e694025bd2381",
+    "url": "/static/media/CheckIn.2fbba9bf.svg"
+  },
+  {
+    "revision": "93bbe92fcb720d736d4ecb77e99324fd",
+    "url": "/static/media/CheckOut.93bbe92f.svg"
+  },
+  {
+    "revision": "2820482464ac773b6005dd8725f40969",
+    "url": "/static/media/Profile.28204824.svg"
+  },
+  {
+    "revision": "9e9e0065a787e6bc62bf7bd268294be4",
+    "url": "/static/media/actionsuccess.9e9e0065.svg"
+  },
+  {
+    "revision": "2c3504f38f0a1863fb3dffbc58dc3d82",
+    "url": "/static/media/next.2c3504f3.svg"
+  },
+  {
+    "revision": "cee59e20324c5b78fac04d2fb9339981",
+    "url": "/static/media/surface.cee59e20.svg"
+  }
+]);
